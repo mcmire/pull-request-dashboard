3601b8436ec6213ee80c1a6eadb5d065ca072675
Elliot Winkler <elliot.winkler@gmail.com>
2021-12-16 10:34:07 -0700

    Add bottom border, hover to each row

--- src/components/PullRequestList.js
+++ src/components/PullRequestList.js
@@ -1,112 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import friendlyTime from 'friendly-time';
-import MetamaskIcon from '../images/metamask-fox.svg';
-import DotIcon from '../images/icons/octicons/dot-16.svg';
-import DotFillIcon from '../images/icons/octicons/dot-fill-16.svg';
 import TriangleDownIcon from '../images/icons/octicons/triangle-down-16.svg';
-import { times } from '../util';
-
-const MAX_PRIORITY_LEVEL = 5;
-const STATUSES_BY_NAME = {
-  hasMergeConflicts: 'Has merge conflicts',
-  hasRequiredChanges: 'Has required changes',
-  hasMissingTests: 'Missing tests',
-  isBlocked: 'Blocked by dependent task',
-  isReadyToMerge: 'Ready to merge',
-};
-
-/**
- * The component that represents a pull request in the search results.
- *
- * @param {object} props - The props for this component.
- * @param {PullRequest} props.pullRequest - The pull request to render.
- * @returns {JSX.Element} The JSX that renders this component.
- */
-function PullRequest({ pullRequest }) {
-  return (
-    <tr>
-      <td className="pr-2 py-2">
-        {pullRequest.isCreatedByMetaMaskian ? (
-          <MetamaskIcon className="h-[1.2em]" />
-        ) : null}
-      </td>
-      <td className="pr-2 py-2">
-        <div className="flex">
-          {pullRequest.authorAvatarUrls.map((url, i) => (
-            <img
-              key={i}
-              src={url}
-              className="rounded-full h-[1.25em] border border-white mr-[-0.5em]"
-            />
-          ))}
-        </div>
-      </td>
-      <td className="pr-2 py-2 text-gray-500">
-        <a
-          href={pullRequest.url}
-          className="hover:text-blue-500 hover:underline"
-        >
-          #{pullRequest.number}
-        </a>
-      </td>
-      <td className="pr-2 py-2 font-semibold">
-        <a
-          href={pullRequest.url}
-          className="hover:text-blue-500 hover:underline"
-        >
-          {pullRequest.title}
-        </a>
-      </td>
-      <td className="pr-2 py-2 text-orange-500">
-        {friendlyTime(pullRequest.createdAt)}
-      </td>
-      <td className="pr-2 py-2">
-        {times(pullRequest.priorityLevel, (i) => (
-          <DotFillIcon key={i} className="inline-block h-[1em]" />
-        ))}
-        {times(MAX_PRIORITY_LEVEL - pullRequest.priorityLevel, (i) => (
-          <DotIcon key={i} className="inline-block h-[1em]" />
-        ))}
-      </td>
-      <td className="pr-2 py-2">
-        <div className="flex items-center">
-          {pullRequest.statuses.map((status, i) => {
-            return (
-              <span
-                key={i}
-                className={classnames(
-                  'inline-block rounded-full text-white py-1.5 px-2.5 text-xs',
-                  {
-                    'bg-black': status === 'isBlocked',
-                    'bg-red-500': status !== 'isBlocked',
-                    'mr-2': i < pullRequest.statuses.length - 1,
-                  },
-                )}
-              >
-                {STATUSES_BY_NAME[status]}
-              </span>
-            );
-          })}
-        </div>
-      </td>
-    </tr>
-  );
-}
-
-PullRequest.propTypes = {
-  pullRequest: PropTypes.shape({
-    isCreatedByMetaMaskian: PropTypes.bool.isRequired,
-    authorAvatarUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
-    number: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    createdAt: PropTypes.instanceOf(Date).isRequired,
-    priorityLevel: PropTypes.number.isRequired,
-    statuses: PropTypes.arrayOf(PropTypes.string).isRequired,
-    url: PropTypes.string.isRequired,
-  }),
-};
+import PullRequestRow from './PullRequestRow';
 
 /**
  * The component for the pull request list.
@@ -146,7 +41,7 @@ export default function PullRequestList({
       <tbody>
         {hasInitiallyLoadedPullRequests ? (
           pullRequests.map((pullRequest, i) => (
-            <PullRequest key={i} pullRequest={pullRequest} />
+            <PullRequestRow key={i} pullRequest={pullRequest} />
           ))
         ) : (
           <tr>
---
+++ src/components/PullRequestRow.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import friendlyTime from 'friendly-time';
+import MetamaskIcon from '../images/metamask-fox.svg';
+import DotIcon from '../images/icons/octicons/dot-16.svg';
+import DotFillIcon from '../images/icons/octicons/dot-fill-16.svg';
+import { times } from '../util';
+
+const MAX_PRIORITY_LEVEL = 5;
+const STATUSES_BY_NAME = {
+  hasMergeConflicts: 'Has merge conflicts',
+  hasRequiredChanges: 'Has required changes',
+  hasMissingTests: 'Missing tests',
+  isBlocked: 'Blocked by dependent task',
+  isReadyToMerge: 'Ready to merge',
+};
+
+/**
+ * Renders a cell.
+ *
+ * @param {object} props - The props for this component.
+ * @param {string} props.className - CSS classes.
+ * @param {JSX.Element} props.children - The children.
+ * @returns {JSX.Element} The JSX that renders this component.
+ */
+function Cell({ className, children, ...rest }) {
+  return (
+    <td
+      className={`${className} pr-2 py-2 border-b group-hover:bg-gray-100`}
+      {...rest}
+    >
+      {children}
+    </td>
+  );
+}
+
+Cell.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+/**
+ * The component that represents a pull request in the search results.
+ *
+ * @param {object} props - The props for this component.
+ * @param {PullRequest} props.pullRequest - The pull request to render.
+ * @returns {JSX.Element} The JSX that renders this component.
+ */
+export default function PullRequest({ pullRequest }) {
+  return (
+    <tr className="group">
+      <Cell>
+        {pullRequest.isCreatedByMetaMaskian ? (
+          <MetamaskIcon className="h-[1.2em]" />
+        ) : null}
+      </Cell>
+      <Cell>
+        <div className="flex">
+          {pullRequest.authorAvatarUrls.map((url, i) => (
+            <img
+              key={i}
+              src={url}
+              className="rounded-full h-[1.25em] border border-white mr-[-0.5em]"
+            />
+          ))}
+        </div>
+      </Cell>
+      <Cell className="text-gray-500">
+        <a
+          href={pullRequest.url}
+          className="hover:text-blue-500 hover:underline"
+        >
+          #{pullRequest.number}
+        </a>
+      </Cell>
+      <Cell className="font-semibold">
+        <a
+          href={pullRequest.url}
+          className="hover:text-blue-500 hover:underline"
+        >
+          {pullRequest.title}
+        </a>
+      </Cell>
+      <Cell className="text-orange-500">
+        {friendlyTime(pullRequest.createdAt)}
+      </Cell>
+      <Cell>
+        {times(pullRequest.priorityLevel, (i) => (
+          <DotFillIcon key={i} className="inline-block h-[1em]" />
+        ))}
+        {times(MAX_PRIORITY_LEVEL - pullRequest.priorityLevel, (i) => (
+          <DotIcon key={i} className="inline-block h-[1em]" />
+        ))}
+      </Cell>
+      <Cell>
+        <div className="flex items-center">
+          {pullRequest.statuses.map((status, i) => {
+            return (
+              <span
+                key={i}
+                className={classnames(
+                  'inline-block rounded-full text-white py-1.5 px-2.5 text-xs',
+                  {
+                    'bg-black': status === 'isBlocked',
+                    'bg-red-500': status !== 'isBlocked',
+                    'mr-2': i < pullRequest.statuses.length - 1,
+                  },
+                )}
+              >
+                {STATUSES_BY_NAME[status]}
+              </span>
+            );
+          })}
+        </div>
+      </Cell>
+    </tr>
+  );
+}
+
+PullRequest.propTypes = {
+  pullRequest: PropTypes.shape({
+    isCreatedByMetaMaskian: PropTypes.bool.isRequired,
+    authorAvatarUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+    number: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    createdAt: PropTypes.instanceOf(Date).isRequired,
+    priorityLevel: PropTypes.number.isRequired,
+    statuses: PropTypes.arrayOf(PropTypes.string).isRequired,
+    url: PropTypes.string.isRequired,
+  }),
+};
