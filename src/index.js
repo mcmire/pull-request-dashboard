ca33df7c419134e7df1d44efaf4f5c9a6243f84b
Elliot Winkler <elliot.winkler@gmail.com>
2021-12-17 22:25:43 -0700

    Don't need this file anymore

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import App from './components/App';
-
-import './globals.css';
-
-render(<App />, document.querySelector('#root'));
